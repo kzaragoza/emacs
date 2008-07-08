94a4d2df52e34d4e7f27e1378c22f83d8259ed59
Kris Zaragoza <kzaragoza@gmail.com>
2008-07-08 15:54:47 -0400

    Updated custom file.

--- custom.el
+++ custom.el
@@ -14,6 +14,7 @@
  '(sgml-basic-offset 4)
  '(speedbar-frame-parameters (quote ((minibuffer) (width . 30) (border-width . 0) (menu-bar-lines . 0) (tool-bar-lines . 0) (unsplittable . t) (left-fringe . 0))))
  '(speedbar-show-unknown-files t)
+ '(tabbar-mode t nil (tabbar))
  '(tool-bar-mode nil)
  '(transient-mark-mode t))
 (custom-set-faces
