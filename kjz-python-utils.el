a38b6f82f2023e6d31e876affcf2f75a9b79081f
Kris Zaragoza <kzaragoza@gmail.com>
2008-09-28 11:12:49 -0400

    Added a couple of commands for Django development. Still requires some tweaking to make general purpose.

--- kjz-python-utils.el
+++ kjz-python-utils.el
@@ -26,3 +26,16 @@ normalizing Python code that has tabs in place for indentation."
 ;; Define a snippet for Django models.
 (yas/define 'python-mode "render"
             "render_to_response(\"${1:template}\", ${2:locals()}, context_instance=RequestContext(request))$0")
+
+;; Define a couple of useful commands for Django development.
+(defun django-start-server ()
+  "Starts the Django development server."
+  (interactive)
+  (start-process "django-dev-server" "*Django Server*" "/Users/kjz/projects/dtg/manage.py" "runserver")
+  (message "Django server started."))
+
+(defun django-stop-server()
+  "Stops the Django development server."
+  (interactive)
+  (signal-process "django-dev-server" 2) ; Send a SIGINT to stop the server.
+  (message "Django server stopped."))
