92260ddd95a2dfd2e2ef0ea6ec8084ef3b9d68e0
Kris Zaragoza <kzaragoza@gmail.com>
2008-12-23 12:25:32 -0500

    General cleanup. Minor tweaks to xml-tidy-buffer function.

--- kjz-text-utils.el
+++ kjz-text-utils.el
@@ -1,12 +1,8 @@
 ;; Utilities and other stuff I like when in text mdoes.
 
-; Wire up longlines-mode so we get some nice word wrapping in text modes.
+; Wire up longlines-mode so we get some nice word wrapping in text modes. This isn't
+; set up in any hooks, so call it manually when wanted.
 (autoload 'longlines-mode "longlines.el" "Minor mode for editing long lines." t)
-;; (add-hook 'text-mode-hook 'longlines-mode)
-;; (add-hook 'sgml-mode-hook 'longlines-mode-off) ; Don't want this in XML/HTML docs.
-
-;; When in Text mode, turn on auto-fill-mode
-;(add-hook 'text-mode-hook (lambda () (auto-fill-mode t)))
 
 ; Load the lorem ipsum generator. Very useful for placeholder text.  We put this
 ; hear rather than in a mode hook because we can use this in all kinds of
@@ -21,17 +17,13 @@
 	      auto-mode-alist))
 (add-hook 'rst-adjust-hook 'rst-toc-update)
 
-; Set the HTML indent value to 4 from the default of 2.
-; (setq html-helper-basic-offset 4)
-
-; Method to call tidy on an XML document.
+; Function to call tidy on an XML document.
+(setq xml-tidy-command "tidy -q -i -xml")
 (defun xml-tidy-buffer ()
   (interactive)
-  (mark-whole-buffer)
-  (shell-command-on-region (point-min) (point-max) "tidy -q -i -xml" nil t))
-
-; Load nxhtml mode stuff
-;(load "nxml/autostart.el")
+  (save-excursion
+    (mark-whole-buffer)
+    (shell-command-on-region (point-min) (point-max) xml-tidy-command nil t)))
 
 ; Get rid of html-helper-mode from the auto-mode-alist
 (setq auto-mode-alist
@@ -40,13 +32,4 @@
 (setq magic-mode-alist
       (remove-if '(lambda (x) (eq (cdr x) 'html-helper-mode)) auto-mode-alist))
 
-; load Django mode for HTML templating stuff.
-;(load "django-html-mode.el")
-;(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mode))
-
-; Add a text mode hook to not indent lines, but rather just tab to the tab stop.
-;; (add-hook 'text-mode-hook
-;;           (lambda ()
-;;             (setq indent-line-function 'tab-to-tab-stop)
-;;             (local-set-key (kbd "RET") 'newline)))
 
