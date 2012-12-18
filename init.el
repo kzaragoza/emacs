58439e2d863092d06c70c8935682e70791bad926
Kris Zaragoza <kris.zaragoza@worldone.com>
2012-12-18 15:24:57 -0500

    Added more package sources.

--- init.el
+++ init.el
@@ -7,6 +7,11 @@
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
 
+;; Add some other archives to the package manager.
+(require 'package)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Custom code and utilities
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
