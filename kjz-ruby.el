2f7aedfa0f0f2bfa6c9f049e39730d113e85c2e9
Kris Zaragoza <kris.zaragoza@worldone.com>
2013-01-21 08:18:37 -0500

    Miscellaneous ruby-mode and related tweaks.

--- kjz-ruby.el
+++ kjz-ruby.el
@@ -1,5 +1,13 @@
 ;; Make sure Rake files open in ruby-mode.
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+
+;; Load rspec-mode for simple running of specs from a buffer.
+(require 'rspec-mode)
+
+;; Load rinari for Rails dev.
+(require 'rinari)
+(global-rinari-mode)
 
 ;; Make the return key do a newline and indent.
 (add-hook 'ruby-mode-hook (lambda () (local-set-key (kbd "RET") 'newline-and-indent)))
