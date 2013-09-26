473b29b48c423ec7c8e2b304af248f430b0d011a
Kris Zaragoza <kris.zaragoza@worldone.com>
2013-09-26 11:52:04 -0400

    Update multiple-cursors key bindings to be less likely to conflict with other modes.

--- kjz-misc.el
+++ kjz-misc.el
@@ -116,10 +116,10 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; Bind keys for multiple-cursors mode.
-(global-set-key (kbd "C-,") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-.") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c C-.") 'mc/mark-all-like-this)
-(global-set-key (kbd "C-c C-l") 'mc/edit-lines)
+(global-set-key (kbd "M-[") 'mc/mark-previous-like-this)
+(global-set-key (kbd "M-]") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-M-]") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-M-[") 'mc/edit-lines)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Utilities and Custom Functions
