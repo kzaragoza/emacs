322251d60df4e364e623741641d164dd8bfc8d4a
Kris Zaragoza <kris.zaragoza@sermo.com>
2015-04-20 09:18:56 -0400

    Set default font to Andale Mono.

--- custom.el
+++ custom.el
@@ -48,7 +48,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Consolas"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Andale Mono"))))
  '(mode-line ((t (:background "#d3f9ee" :foreground "#081724" :inverse-video nil :box (:line-width 1 :color "#d3f9ee")))))
  '(mode-line-inactive ((t (:inherit mode-line :background "#1d5483" :foreground "#081724" :inverse-video nil :box (:line-width 1 :color "#1d5483") :weight light))))
  '(powerline-active1 ((t (:inherit mode-line :background "grey22" :foreground "gray100")))))
