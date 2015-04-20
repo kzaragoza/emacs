f13cd4d15f777e695686ff27979309d4b5689af0
Kris Zaragoza <kris.zaragoza@sermo.com>
2015-04-20 17:28:09 -0400

    Update utility to export HTML directly to the clipboard.
    This approach preserves the HTML formatting. Since my email and other
    tools can read this format directly, no formatting (like tables) gets
    lost.

--- lisp/kjz-misc.el
+++ lisp/kjz-misc.el
@@ -190,8 +190,27 @@ the kill ring."
 	  (select-window first-win)
 	  (if this-win-2nd (other-window 1))))))
 
-(defun kjz-org-export-rich-text (p1 p2)
-  "Quick utility script to export Org data to RTF to paste into other applications."
-  (interactive "r")
-  (message "Region is %d to %d" p1 p2)
-  (shell-command-on-region p1 p2 "pandoc -f org -t rtf --standalone | pbcopy"))
+
+(defun kjz-org-export-rich-text ()
+  "Quick utility script to export Org data to HTML on the
+clipboard to paste into other applications."
+  (interactive)
+  (org-html-export-as-html)
+  ;; Note we have to tweak the process coding system. It's set to Latin-1 by
+  ;; default, which completely trashes the chevrons in the string, making the
+  ;; AppleScript syntax invalid.
+  ;;
+  ;; The process here is simple.
+  ;;
+  ;;   1. Let org-mode do its export thing to an HTML buffer.
+  ;;
+  ;;   2. Convert the buffer text into a string of hex digits, 2 characters for
+  ;;   each buffer char.
+  ;;
+  ;;   3. Use AppleScript to set the HTML format version of the clipboard
+  ;;   contents. Unfortunately, pbpaste won't do this.
+  (let* ((default-process-coding-system '(undecided-unix . utf-8-unix))
+         (buf (apply #'concat (map 'list (lambda (c) (format "%0.2x" c)) (buffer-string))))
+         (cmd (concat "osascript -e 'set the clipboard to «data HTML" buf "»'")))
+    (shell-command cmd)
+    (kill-buffer)))
