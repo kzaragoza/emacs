67b5d4bedbb608b57f9d4e0b0132568df5e9aa0e
Kris Zaragoza <kzaragoza@gmail.com>
2016-09-17 17:21:34 -0400

    Update Lisp config.
    * Set up for using SBCL with SLIME for Common Lisp work.

--- lisp/kjz-lisp.el
+++ lisp/kjz-lisp.el
@@ -1,8 +1,2 @@
-;; Set up Clojure support.
-;; (add-to-list 'load-path "~/.emacs.d/vendor/clojure-mode")
-;; (setq inferior-lisp-program "~/bin/clj")
-;; (require 'clojure-mode)
-;; (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-;; (add-hook 'clojure-mode-hook
-;;           '(lambda ()
-;;              (define-key clojure-mode-map "\C-c\C-e" 'lisp-eval-last-sexp)))
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+(setq slime-contribs '(slime-fancy))
