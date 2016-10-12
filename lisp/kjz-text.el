b3a23d16232e35dfb459e2fc8bcec9c36e77f4c5
Kris Zaragoza <kris.zaragoza@sermo.com>
2016-10-12 11:47:59 -0400

    Load markdown-mode for API Blueprint

--- lisp/kjz-text.el
+++ lisp/kjz-text.el
@@ -1,11 +1,11 @@
 ;; Utilities and other stuff I like when in text mdoes.
 
-; Load the lorem ipsum generator. Very useful for placeholder text.  We put this
-; hear rather than in a mode hook because we can use this in all kinds of
-; documents, including code.
+;; Load the lorem ipsum generator. Very useful for placeholder text.  We put this
+;; hear rather than in a mode hook because we can use this in all kinds of
+;; documents, including code.
 (require 'lorem-ipsum)
 
-; Set up reStructuredText support for text documents.
+;; Set up reStructuredText support for text documents.
 (autoload 'rst-mode "rst.el" "Major mode for reStructuredText documents." t)
 (setq auto-mode-alist
       (append '(("\\.rst$" . rst-mode)
@@ -13,7 +13,7 @@
 	      auto-mode-alist))
 (add-hook 'rst-adjust-hook 'rst-toc-update)
 
-; Function to call tidy on an XML document.
+;; Function to call tidy on an XML document.
 (setq xml-tidy-command "tidy -q -i -xml -wrap 75")
 (defun xml-tidy-buffer ()
   (interactive)
@@ -21,13 +21,15 @@
     (mark-whole-buffer)
     (shell-command-on-region (point-min) (point-max) xml-tidy-command nil t)))
 
-; Get rid of html-helper-mode from the auto-mode-alist
+;; Get rid of html-helper-mode from the auto-mode-alist
 (setq auto-mode-alist
       (cl-remove-if '(lambda (x) (eq (cdr x) 'html-helper-mode)) auto-mode-alist))
 
 (setq magic-mode-alist
       (cl-remove-if '(lambda (x) (eq (cdr x) 'html-helper-mode)) auto-mode-alist))
 
-
 ;; Have Emacs recognize a single space as a sentence end.
 (setq sentence-end-double-space nil)
+
+;; Load up markdown-mode when editing API Blueprint files.
+(push '("\\.apib$" . markdown-mode) auto-mode-alist)
