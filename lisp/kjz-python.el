79468510d3339b8e95512a947ac89e5a6b4ed07d
Kris Zaragoza <kris.zaragoza@sermo.com>
2018-05-07 17:51:16 -0400

    Clean up Python configuration.

--- lisp/kjz-python.el
+++ lisp/kjz-python.el
@@ -1,27 +1,13 @@
-;; Set up some stuff for Python mode
-;; (autoload 'python-mode "python-mode" "Python editing mode." t)
-;; (setq auto-mode-alist
-;;       (append '(("\\.py$" . python-mode)) auto-mode-alist))
-;; (setq interpreter-mode-alist
-;;       (append '(("python" . python-mode)) interpreter-mode-alist))
-
-;; Add iPython support.
-;(require 'ipython)
+;; Set up support for Python development.
+(use-package python
+  :mode ("\\.py\\'" . python-mode)
+  :interpreter ("python" . python-mode))
 
 ;; Set up the Speedbar to recognize whether there is an up-to-date object file
 ;; for the given python script.
 (add-hook 'speedbar-load-hook
-	  (lambda ()
-	    (setq speedbar-obj-alist (append '(("\\.py$" . ".pyc") ("\\.py$" . ".pyo")) speedbar-obj-alist))))
-
-(defun convert-tabs-to-spaces ()
-"Finds all tab characters from the point forward to end of buffer,
-replacing them with four (4) spaces. This is most useful for
-normalizing Python code that has tabs in place for indentation."
-  (interactive)
-  (while (search-forward "	" nil t)
-    (replace-match "    " nil t)))
-
+          (lambda ()
+            (setq speedbar-obj-alist (append '(("\\.py$" . ".pyc") ("\\.py$" . ".pyo")) speedbar-obj-alist))))
 
 ;; Define a couple of useful commands for Django development.
 (defun django-start-server ()
@@ -46,4 +32,3 @@ normalizing Python code that has tabs in place for indentation."
    ((member "manage.py" (directory-files root)) (expand-file-name root)) ; Found it. Return the directory name.
    ((equal (expand-file-name root) "/") nil) ; Hit root directory. Return nil.
    (t (django-find-project-root (concat (file-name-as-directory root) ".."))))) ; Recurse and try parent directory.
-
