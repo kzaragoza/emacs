a3fa6824e6d092a1d09e6a71c28a546cc9ab36dc
Kris Zaragoza <kris.zaragoza@sermo.com>
2018-05-07 17:59:52 -0400

    Set up SLIME with use-package.

--- lisp/kjz-lisp.el
+++ lisp/kjz-lisp.el
@@ -1,2 +1,6 @@
-(setq inferior-lisp-program "/usr/local/bin/sbcl --no-linedit")
-(setq slime-contribs '(slime-fancy))
+;; Load SLIME and set up my local SBCL as the default Lisp.
+(use-package slime
+  :ensure t
+  :config (progn
+            (setq inferior-lisp-program "/usr/local/bin/sbcl --no-linedit")
+            (setq slime-contribs '(slime-fancy))))
