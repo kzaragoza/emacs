ea6fcda15a28bee7a1266350ef3c46344063b79e
Kris Zaragoza <kzaragoza@gmail.com>
2024-04-03 13:40:15 -0400

    Clean up some unneeded configuration from an old job.

--- lisp/kjz-org.el
+++ lisp/kjz-org.el
@@ -22,16 +22,12 @@
        (sql . t)))
     ;; Use org-capture for quick entry of items.
     (setq org-default-notes-file (concat org-directory "/inbox.org"))
-    (setq org-work-notes-file (concat org-directory "/sermo.org"))
     (setq org-capture-templates
-          '(("t" "Todo" entry (file+datetree org-work-notes-file)
+          '(("t" "Todo" entry (file+datetree org-default-notes-file)
              "* TODO %?\n  %i"
              :empty-lines 1)
             ("i" "General Item" entry (file+headline org-default-notes-file "Item")
-             "* %?\n  %i")
-            ("w" "Work Item" entry (file+datetree org-work-notes-file)
-             "* %?\n %i"
-             :empty-lines 1)))
+             "* %?\n  %i")))
 
     ;; Hide the leading asterisks and some markup to reduce the visual noise.
     (setq org-hide-leading-stars t)
@@ -68,20 +64,20 @@ clipboard to paste into other applications."
       (kill-buffer buf))))
 
 ;; Set up Deft to do fast searches through my org mode files.
-(use-package deft
-  :ensure t
-  :bind ("<f8>" . 'deft)
-  :config
-  (setq deft-directory "~/org")
-  (setq deft-recursive t)
-  (setq deft-use-filter-string-for-filename t)
-  (setq deft-file-naming-rules '((noslash . "-")
-                                 (nospace . "-")
-                                 (case-fn . downcase)))
-  (setq deft-org-mode-title-prefix t)
-  (setq deft-default-extension "org"))
+;; (use-package deft
+;;   :ensure t
+;;   :bind ("<f8>" . 'deft)
+;;   :config
+;;   (setq deft-directory "~/org")
+;;   (setq deft-recursive t)
+;;   (setq deft-use-filter-string-for-filename t)
+;;   (setq deft-file-naming-rules '((noslash . "-")
+;;                                  (nospace . "-")
+;;                                  (case-fn . downcase)))
+;;   (setq deft-org-mode-title-prefix t)
+;;   (setq deft-default-extension "org"))
 
-;; Patch due to Deft using a deprecated Org Mode function.
-(defun org-open-file-with-emacs (path)
-  "Deft compatability shim to support opening Org links with more recent org-mode versions."
-    (org-open-file path t))
+;; ;; Patch due to Deft using a deprecated Org Mode function.
+;; (defun org-open-file-with-emacs (path)
+;;   "Deft compatability shim to support opening Org links with more recent org-mode versions."
+;;     (org-open-file path t))
