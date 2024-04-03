2218daad6ac73348a1c9418bc35d340b5bf88d1c
Kris Zaragoza <kzaragoza@gmail.com>
2024-04-03 14:04:42 -0400

    Add Clojure supporting packages.

--- lisp/kjz-lisp.el
+++ lisp/kjz-lisp.el
@@ -4,3 +4,11 @@
   :config (progn
             (setq inferior-lisp-program "sbcl --no-linedit")
             (setq slime-contribs '(slime-fancy))))
+
+(use-package clojure-mode
+  :ensure t)
+
+(use-package cider
+  :after clojure-mode
+  :ensure t)
+
